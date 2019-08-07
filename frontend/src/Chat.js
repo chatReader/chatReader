153bfa779bdc5ad1d9aab64388b5b3bf9c34d41a
vanceleon <vanceleon44@gmail.com>
2019-08-07 11:47:42 -0400

    front end has rendering error created chat component

---
+++ frontend/src/Chat.js
@@ -0,0 +1,29 @@
+// import React, { Component } from 'react';
+// // import axios from 'axios';
+
+// class Chat extends Component {
+//   constructor() {
+//     super();
+//     this.state = {
+//       message: {
+//         time: Date.now(),
+//         message: '',
+//       },
+//     };
+//   }
+//   render() {
+//     return <div className='chat-container'>Chat</div>;
+//   }
+// }
+// export default Chat;
+
+import React, { Component } from 'react';
+// import Button from './Button'; // Import a component from another file
+
+class DangerButton extends Component {
+  render() {
+    return <div className="test">test</div>;
+  }
+}
+
+export default DangerButton;
