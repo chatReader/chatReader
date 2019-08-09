98b0de344c3b68361300b2164a10a3548739634a
Vance Leon <vanceleon44@gmail.com>
2019-08-08 20:06:59 -0400

    adding context and code to the index.js file '

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,13 @@
-import React from 'react';
+import React, { useReducer, createContext } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { ContextProvider } from './Content.js';
 
-
-ReactDOM.render(<App />, document.getElementById('root'));
+const rootElement = document.getElementById('root');
+ReactDOM.render(
+  <ContextProvider>
+    <App />
+  </ContextProvider>,
+  rootElement
+);
